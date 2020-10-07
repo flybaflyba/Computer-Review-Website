f77b16ef6685192822652cd5cacc6441af917cdf
Litian Zhang <litian_zhang17@163.com>
2020-10-07 06:07:08 +0000

    Add notes to routes about what HTTP verb each action call and what are the url

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -3,15 +3,20 @@ class ComputersController < ApplicationController
   # call load_computer before every pubice method get called, expect...
   before_action :load_computer, except: [:index, :new, :create]
   
+  # stating which HTTP verb and what URL will call that method.
   
+  
+  # GET - /computers
   def index
     @computers = Computer.all
   end
   
+  # GET - /computers/new
   def new
     @computer = Computer.new 
   end
   
+  # POST - /computers
   def create
     @computer = Computer.new computer_params
     if @computer.save
@@ -21,10 +26,12 @@ class ComputersController < ApplicationController
     end
   end
   
+  # GET - /computers/:id/edit
   def edit
     # @computer = Computer.find params[:id]
   end
   
+  # PATCH/PUT - /computers/:id
   def update
     # @computer = Computer.find params[:id]
     if @computer.update computer_params
@@ -34,12 +41,14 @@ class ComputersController < ApplicationController
     end
   end
   
+  # DELETE - /computers/:id
   def destroy
     # @computer = Computer.find params[:id]
     @computer.destroy
     redirect_to @computer, alert: "Computer Deleted."
   end
   
+  # GET - /computers/:id
   def show
     # @computer = Computer.find params[:id]
     
