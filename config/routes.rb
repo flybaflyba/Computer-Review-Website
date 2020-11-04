37e144eab5cc191980c7c9154a5a01c481a13919
Litian Zhang <litian_zhang17@163.com>
2020-11-04 03:47:54 +0000

    Add sign up and login in button | Change sign up link to standard

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   
   
-  get 'users/index'
-  # get 'reviews/index'
-  get 'computers/index'
+  # get 'users/index'
+  # # get 'reviews/index'
+  # get 'computers/index'
   
+  get 'signup' => 'users#new'
   resources :users
   # get computers/6/reviews/3
   resources :computers do 
