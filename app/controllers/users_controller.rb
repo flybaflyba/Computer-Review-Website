558e1b27c616f50cafdae18d0e7a84b87fac8d15
Litian Zhang <litian_zhang17@163.com>
2020-11-04 05:49:45 +0000

    Add update account page, and customize create account and update account button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+
+  
   def index
   end
   
@@ -20,11 +23,33 @@ class UsersController < ApplicationController
     @user = current_user
   end
   
+  def edit
+    @user = current_user
+  end
+  
+  # PATCH/PUT - /computers/:id
+  def update
+    # @computer = Computer.find params[:id]
+    if @computer.update computer_params
+      redirect_to @computer, notice: "Computer Updated."
+    else
+      render :edit
+    end
+  end
+  
+  # DELETE - /computers/:id
+  def destroy
+    # @computer = Computer.find params[:id]
+    @computer.destroy
+    redirect_to @computer, alert: "Computer Deleted."
+  end
+  
   private 
   
   def user_params
     params.require(:user).permit(:username,:email,:password,:password_confirmation,:firstname,:lastname)
   end
   
+
   
 end
